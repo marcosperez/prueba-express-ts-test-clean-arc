d3a70e90665f4246939906086fca24b7a871ff6c
Marcos Daniel Perez <marcos.d.perez@gmail.com>
2022-06-20 18:18:49 -0300

    fix ping controller test

--- src/__tests__/contexts/controllers/users/Ping.controller.spec.ts
+++ src/__tests__/contexts/controllers/users/Ping.controller.spec.ts
@@ -3,13 +3,16 @@ import { createApp } from "../../../../app";
 import { prismaMock } from "../../../helpers/prisma.mock";
 
 describe("Ping Controller", () => {
-  test("Ping succefuly", async () => {
-    const result = await request(createApp(prismaMock))
+  test("Ping succefuly", (done) => {
+    request(createApp(prismaMock))
       .get("/ping")
       .expect("Content-Type", /json/)
-      .expect(200);
-
-    expect(result.body.message).toBe("pong");
-    expect(result.body.status).toBeTruthy();
+      .expect(200)
+      .then((response) => {
+        expect(response.body.message).toBe("pong");
+        expect(response.body.status).toBeTruthy();
+        done();
+      })
+      .catch((err) => done(err));
   });
 });
